022c488002a8f81bdee3dc968a59d53b54f88cdc
santosfk <santosfk@gmail.com.br>
2025-02-16 21:58:44 -0300

    atualizando configuração do Droplet Jenkins e adicionando cluster Kubernetes

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "2.48.2"
     }
   }
@@ -12,9 +12,22 @@ data "digitalocean_ssh_key" "pc" {
 }
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "jenkins" {
-  image   = "ubuntu-20-04-x64"
-  name    = "jenkins"
-  region  = "nyc2"
-  size    = "s-1vcpu-1gb"
+  image    = var.jenkins_image
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-1vcpu-2gb"
   ssh_keys = [data.digitalocean_ssh_key.pc.id]
+}
+
+resource "digitalocean_kubernetes_cluster" "primary" {
+  name   = "primary"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.32.1-do.0"
+
+  node_pool {
+    name       = "apps"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
 }
